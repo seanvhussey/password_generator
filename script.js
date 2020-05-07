db8e393b4d53ad6d24d1c7aa3d67bafebf70075c
Sean <seanvhussey@outlook.com>
2020-05-06 23:26:51 -0500

    commit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-// Asks the user how long they would like their password to be.
-function create_password(length)
-
 // Characters put into the combined array if the user wants their password to contain numbers.
 var numbers = "0123456789";
 
@@ -14,30 +10,52 @@ var lowercase = "abcdefghijklmnopqrstuvwxwz";
 // Characters put into the combined array if the user wants their password to contain special characters
 var characters = "!@#$%&*+=?";
 
-prompt("Would you like your password to contain numbers?")
-let userchoice = yes(true), no(false);
-if (true)(array + numbers);
-if (false)(array);
+// Empty array to contain selected charsets.
+var array = "";
 
-prompt("Would you like your password to contain uppercase letters?")
-let userchoice = yes(true), no(false);
-if (true)(array + uppercase);
-if (false)(array);
+// How long does the user want the password to be.
+function user_input() {
+  prompt("How long would you like your password to be?");
+  var password_length = user_input.value("Please enter a number value.");
+  console.log(password_length);
+}
 
-prompt("Would you like your password to contain lowercase letters?")
-let userchoice = yes(true), no(false);
-if (true)(array + lowercase);
-if (false)(array);
+// Would the user like numbers included?
+function user_input() {
+  alert("Would you like your password to contain numbers?");
+  var number_confirm = confirm("click okay to confirm.");
+  if (number_confirm === true) {
+    array = array + numbers;
+  }
 
-prompt("Would you like your password to contain special characters?")
-let userchoice = yes(true), no(false);
-if (true)(array + characters)
-if (false)(array);
+  //   Would the user like uppercase letters?
+  alert("Would you like your password to contain uppercase letters?");
+  var uppercase_confirm = confirm("click okay to confirm.");
+  if (uppercase_confirm === true) {
+    array = array + uppercase;
+  }
 
-for (var x = 0; x < create_password.length; x++)
+  //   Would the user like lowercase letters?
+  alert("Would you like your password to contain lowercase letters?");
+  var lowercase_confirm = confirm("click okay to confirm.");
+  if (lowercase_confirm === true) {
+    array = array + lowercase;
+  }
 
-var array = ""
+  //   Would the user like special characters?
+  alert("Would you like your password to contain uppercase letters?");
+  var character_confirm = confirm("click okay to confirm.");
+  if (character_confirm === true) {
+    array = array + characters;
+  }
+  //   Returns the finished dataset.
+  return array;
+}
 
-var generated = Math.random()(array.length)
+var array = user_input();
+for (x = 0; x < array(password_length)[Math.random]; x++) {}
 
-
+function generatePassword() {
+  password_length.user_input.value;
+}
+var whyNotworking = idontknow;
